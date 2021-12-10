07991aa8632fabf005c1f00234f4251e522df200
Michael Rockhold <michael@appel-rockhold.org>
2021-12-10 12:51:35 -0800

    Initial Commit

---
+++ TwelveCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwelveCalc
+//
+//  Created by Michael Rockhold on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue // TODO: Proof we have loaded
+    }
+
+
+}
+
